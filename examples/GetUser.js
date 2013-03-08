dc5b42cabb55538d119139af0f3e36da74861ffb
Wanasit Tanakitrungruang <berrysboy@gmail.com>
2013-03-08 21:15:42 +0900

    Add a new case

--- examples/GetUser.js
+++ examples/GetUser.js
@@ -1,6 +1,6 @@
 
 
-var authToken = "S=s48:U=12be903:E=1449cb52258:C=13d4503f658:P=1cd:A=en-devtoken:V=2:H=02728126921fe27ce2376f555d85f41b"; //The token
+var authToken = "EVERNOTE_TOKEN"; //The token
 
 var config   = require('../config.js');
 var Evernote = require('../evernode').Evernote;
@@ -14,7 +14,7 @@ var evernote = new Evernote(
 
 var offset 		= 0;
 var count 		= 10;
-var words 		= '';
+var words 		= ''; // Key Words
 var sortOrder = 'UPDATED' // 'CREATED';
 var ascending = false;
 
@@ -22,13 +22,6 @@ var ascending = false;
 evernote.getUser(authToken,function(err, edamUser) {
 	
 	if (err)  return console.log(err);
-  
-  //Find the notes....
-  evernote.findNotes(edamUser,  words, { offset:offset, count:count, sortOrder:sortOrder, ascending:ascending }, function(err, noteList) {
-    
-    if (err)  return console.log(err);
-    
-    return console.log(noteList);
-  });
+  return console.log(edamUser);
 });
 
